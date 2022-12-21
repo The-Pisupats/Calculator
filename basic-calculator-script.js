e6c30508a66be3a4ab70c235d1783c39b0b67fee
VenkataP <venkatasreeman@gmail.com>
2022-12-21 15:47:47 -0600

    add code to avoid multiple decimals

--- basic-calculator-script.js
+++ basic-calculator-script.js
@@ -2,11 +2,15 @@
 
 let workingAreaEle = document.querySelector("#working-area-txt");
 
+let firstNumber;
+let secondNumber;
+let operator;
+
 document.querySelector("#AC-btn").addEventListener("click", function() {
     workingAreaEle.value = "0";
 })
 
-let allDigitButtons = document.querySelectorAll(".digit-btn");
+let allDigitBtns = document.querySelectorAll(".digit-btn");
 
 let addValueToWorkingArea = function(event){
 
@@ -15,11 +19,34 @@ let addValueToWorkingArea = function(event){
     if(workingAreaEle.value == "0" && valueEntered !== "."){
         workingAreaEle.value = valueEntered;
     }else{
+        if(workingAreaEle.value.includes(".") && event.srcElement.innerText == (".")){
+            return;
+        }
         workingAreaEle.value = workingAreaEle.value + valueEntered;
     }
     
 }
 
-for(let i = 0; i < allDigitButtons.length; i++){
-    allDigitButtons[i].addEventListener("click", addValueToWorkingArea);
+let performOperation = function(event) {
+    let srcEle = event.srcElement;
+    console.log(srcEle.innerText);
+
+    if(srcEle.innerText == "="){
+        workingAreaEle.value = eval(workingAreaEle.value);
+    }else if(srcEle.innerText == "+/-"){
+        workingAreaEle.value = eval(workingAreaEle.value) * -1;
+    }else{
+        workingAreaEle.value = workingAreaEle.value + srcEle.innerText;
+    }
+
+}
+
+for(let i = 0; i < allDigitBtns.length; i++){
+    allDigitBtns[i].addEventListener("click", addValueToWorkingArea);
+}
+
+let allOperatorBtns = document.querySelectorAll(".operator-btn");
+
+for (let i = 0; i < allOperatorBtns.length; i++) {
+    allOperatorBtns[i].addEventListener("click", performOperation);
 }
